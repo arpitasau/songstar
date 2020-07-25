a933e8e2442bfa4595941c57a72984c2e459ee17
Arpita K Sau <arpitakaran@gmail.com>
2020-07-25 13:30:36 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootarpita1",
+    database: "top_songsDB"
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log(`connected on port ${connection.threadId}`)
+    initialPrompts()
+});
+
+function initialPrompts() {
+    inquirer.prompt([
+        {
+            name: "action",
+            message: "what do you want to do?",
+            type: "list",
+            choices: [
+                "ARTIST SEARCH",
+                "MULTI SEARCH",
+                "RANGE SEARCH",
+                "SONG SEARCH",
+                "EXIT"
+            ]
+        }
+    ]).then(answer => {
+        switch (answer.action) {
+            case "ARTIST SEARCH":
+                artistSearch();
+                break;
+            case "MULTI SEARCH":
+                multiSearch();
+                break;
+            case "RANGE SEARCH":
+                rangeSearch();
+                break;
+            case "SONG SEARCH":
+                songSearch();
+                break;
+            default:
+                connection.end()
+                process.exit()
+        }
+    })
+}
+
+//artist search
+function artistSearch() {
+    console.log("searching artist...");
+    initialPrompts()
+}
+
+//A query that returns all artists who appear within the top 5000 more than once
+function multiSearch() {
+    console.log("searching multisearch")
+    initialPrompts()
+
+}
+
+//Aquery that returns all data contained within a specific range
+function rangeSearch() {
+    console.log("searching range");
+    initialPrompts()
+}
+
+function songSearch() {
+    console.log("searching song");
+    initialPrompts()
+}
